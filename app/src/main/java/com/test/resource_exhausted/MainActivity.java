fb3dde54d5ed030433923ad8d26c9839d5b0921e
Mauratay <francisco.mauratay@gmail.com>
2021-10-27 13:12:55 -0500

    Adding repo

---
+++ app/src/main/java/com/test/resource_exhausted/MainActivity.java
@@ -0,0 +1,41 @@
+package com.test.resource_exhausted;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getCacheDir().delete();
+    }
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    Task<QuerySnapshot> docRef = db.collection("testing_res_exh").document("00Unr0bfNkHsXqiXMZMe").collection("res_exh")
+            .limit(100                                      )
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            getCacheDir().delete();
+                            Log.d("TAG", document.getId() + " => " + document.getData());
+                        }
+                    } else {
+                        Log.d("TAG", "Error getting documents: ", task.getException());
+                    }
+                }
+            });
+}
